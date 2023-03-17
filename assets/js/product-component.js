bf9ef0954705095aba161297dae660504190677d
Kostas Tsalikidis <tsalikidhs@gmail.com>
2023-03-17 09:27:41 +0200

    fetch data of a product-component

--- assets/js/product-component.js
+++ assets/js/product-component.js
@@ -1,13 +1,28 @@
 export default {
   props: [
-    'foo',
+    'id',
   ],
+  created() {
+    this.loading = true;
+
+    fetch('orderchamp product page.json').then((response) => {
+      response.json().then((data) => {
+        this.loading = false;
+      });
+    });
+  },
   data() {
     return {
-      
+      loading: false,
+
     };
   },
   template: `
-    <div class="product">{{foo}}</div>
+    <div class="product">
+      <span v-if="loading">Please Wait</span>
+      <template v-else="!loading">
+        product preview
+      </template>
+    </div>
   `,
 }

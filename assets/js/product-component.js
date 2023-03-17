cf597a6d7af2d6f7392aec7aad240b2037cd55ef
Kostas Tsalikidis <tsalikidhs@gmail.com>
2023-03-17 10:07:41 +0200

    add images preview on product-component

--- assets/js/product-component.js
+++ assets/js/product-component.js
@@ -8,22 +8,42 @@ export default {
     fetch('orderchamp product page.json').then((response) => {
       response.json().then((data) => {
         this.loading = false;
+        this.images = data.data.storefrontBySlug.listing.images.edges.map((record) => {
+          return {
+            thumbnailUrl: record.node.small,
+            url: record.node.big,
+          };
+        });
+        this.imageSelected = this.images[0];
       });
     });
   },
   data() {
     return {
       loading: false,
-
+      images: [],
+      imageSelected: null,
     };
+  },
+  methods: {
+    onImageSelect(index) {
+      this.imageSelected = this.images[index];
+    }
   },
   template: `
     <div class="product">
       <span v-if="loading">Please Wait</span>
       <template v-else="!loading">
-        <div class="thumbnails">thumbnails</div>
+        <div class="thumbnails">
+          <div
+            v-for="(image, index) in images"
+            class="thumbnail"
+            :style="{ 'background-image': 'url(' + image.thumbnailUrl + ')' }"
+            @click="onImageSelect(index)"
+          ></div>
+        </div>
         <div class="left">
-          <div class="image">image</div>
+          <div class="image" :style="{ 'background-image': 'url(' + imageSelected.url + ')' }"></div>
           <div class="description">description</div>
         </div>
         <div class="right">
